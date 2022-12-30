1cd9c9a3313decc80b7fbef6351732f2e38856b8
mantovaniarts <mantovaniarts@hotmail.com>
2022-12-30 15:22:56 -0500

    html yo json

--- components/crud/common/HtmlToJson.vue
+++ components/crud/common/HtmlToJson.vue
@@ -1,32 +1,69 @@
 <template lang="">
-  <section class="flex">
-    <div id="page">
-    </div>
-
-    {{ model }}
+  <section class="flex gap-4">
+    <pre  id="htmlPage" class="page w-3/4 border" contenteditable="true" @input="convert">
+    </pre>
+    <div class="w-1/4">
+      <JsonEditor  
+        mode="tree"
+        v-model:text="model" 
+      />
+    </div> 
   </section>
 </template>
 
 <script setup> 
-const model = ref({})
-onMounted(() => {
-  let doc = [...document.getElementById('page').children]
+const model = ref('[]')
+const ignore = ['script','!--','#comment']
 
-  function getTagJson(tag){
-    let children = [...tag.children].map(i => getTagJson(i))
-    return {
-      "$el": tag.tagName.toLowerCase(),
-      "attrs": [...tag.attributes].reduce(
-        (pv, i, k) => (pv = {...pv, [i.name]: i.value}),
-        ({})
-      ),
-      children
-    }
+function checkTextValue (node) {
+  let nodeName = node?.nodeName?.toLowerCase()
+  let nodeValue = node?.nodeValue?.trim().replace('\\n', '')
+  if( ignore.includes(nodeName) ) return false
+  if( nodeName == '#text' && !nodeValue ) return false
+  return true
+}
+
+function getTagJson(tag){  
+  let children = []
+  let childs = [...tag.childNodes]
+  let tagName = tag?.nodeName?.toLowerCase()
+ 
+  if( childs.length > 0  ){ 
+    childs.forEach(i => { 
+      if( checkTextValue(i) ) children.push(getTagJson(i))
+    })    
+  } 
+
+  if( tagName == '#text' )
+    return tag.nodeValue
+
+  return {
+    "$el": tagName,
+    "attrs": [...(tag.attributes ?? [])].reduce(
+      (pv, i, k) => (pv = {...pv, [i.name]: i.value}),
+      ({})
+    ),
+    children
   }
+}
 
-  model.value = doc.map(i => getTagJson(i))
-  console.log(
-    model.value
-  )
+var stringToHTML = function (str) {
+	var dom = document.createElement('div');
+	dom.innerHTML = str;
+	return dom;
+}
+
+function convert (input) {   
+  let childs = []
+  let base = stringToHTML(input?.target?.innerText)
+  let doc = [...base.childNodes]
+ 
+  childs = doc.filter(i => checkTextValue(i)).map(i => getTagJson(i))
+  // console.log(childs)
+  model.value = JSON.stringify(childs)
+  // console.log(model.value)
+}
+
+onMounted(() => {
 })
 </script>
